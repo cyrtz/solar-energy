d245cfc2ed9aaa43ccb74db52127c7567e4f4795
cyrtz <cys20030203@gmail.com>
2024-01-17 13:25:24 +0800

    登入畫面第一版

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -22,7 +26,9 @@ import { MatButtonModule } from '@angular/material/button';
     ReactiveFormsModule,
     MatCardModule,
     MatTabsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,26 +1,57 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
-  loginForm: FormGroup;
+export class LoginComponent implements OnInit{
+
+  signinForm: FormGroup;
   signupForm: FormGroup;
+  // signinForm: FormGroup | undefined;
+  // signupForm: FormGroup | undefined;
+
   constructor(private fb: FormBuilder) {
-    this.loginForm = this.fb.group({
+    this.signinForm = this.fb.group({
+      memberList: this.fb.array([]),
       username: ['', Validators.required],
       password: ['', Validators.required],
     });
     this.signupForm = this.fb.group({
+      memberList: this.fb.array([]),
       username: ['', Validators.required],
       password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
     });
+  
+  }
+
+  ngOnInit(): void {
+
+    // this.signinForm = this.fb.group({
+    //     memberList: this.fb.array([]),
+    //     username: ['', Validators.required],
+    //     password: ['', Validators.required],
+    //   });
+    //   this.signupForm = this.fb.group({
+    //     memberList: this.fb.array([]),
+    //     username: ['', Validators.required],
+    //     password: ['', Validators.required],
+    //   });
+
   }
   onSubmit(): void {
-    // 在這裡處理表單提交的邏輯
-    console.log(this.loginForm.value);
+    console.log(this.signinForm?.value);
+  }
+  // 取得 FormArray
+  get formArray(): FormArray {
+    return this.signinForm?.get('memberList')! as FormArray;
+  }
+  // 判斷表單是否無效
+  get isFormInvalid(): boolean {
+    return this.formArray.controls.length ===0 || this.signinForm!.invalid;
   }
 }
