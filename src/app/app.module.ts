20b33e755146772ac36c04af95ed2e2c37356886
cyrtz <cys20030203@gmail.com>
2024-01-22 16:51:57 +0800

    新增一些套件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { DialogComponent } from './dialog/dialog.component';
+import { DeviceDetailComponent } from './device-detail/device-detail.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDividerModule } from '@angular/material/divider';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { MatTableModule } from '@angular/material/table';
+// import { DeviceManageComponent } from './service/device-manage/dvice-manage.component';
+import { DeviceManageComponent } from './device-manage/device-manage.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    DialogComponent,
+    DeviceDetailComponent,
+    DeviceManageComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +46,16 @@ import { MatInputModule } from '@angular/material/input';
     MatTabsModule,
     MatButtonModule,
     MatInputModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatInputModule,
+    MatIconModule,
+    MatToolbarModule,
+    HttpClientModule,
+    MatDialogModule,
+    MatProgressBarModule,
+    MatDividerModule,
+    NgApexchartsModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
