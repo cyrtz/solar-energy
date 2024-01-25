1b38a51f9ecd097398e3f4c22b51cfb1ab56f990
cyrtz <cys20030203@gmail.com>
2024-01-25 10:50:03 +0800

    新增設備彈跳視窗

--- src/app/new-device-dialog/new-device-dialog.component.ts
+++ src/app/new-device-dialog/new-device-dialog.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { DeviceManageService } from '../service/device-manage.service';
+import { INewDeviceRequest } from '../models/device-manage';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-new-device-dialog',
   templateUrl: './new-device-dialog.component.html',
   styleUrls: ['./new-device-dialog.component.scss']
 })
+
 export class NewDeviceDialogComponent {
 
+  // 定義一個事件發布器
+  @Output() dialogClosed = new EventEmitter<void>();
+
+  newDiveceForm = new FormGroup({
+    devName: new FormControl(''),
+    address: new FormControl(''),
+    place: new FormControl(''),
+  })
+
+  constructor(
+    private deviceService: DeviceManageService,
+    public dialog: MatDialog,
+  ) { }
+
+  // 新增設備
+  add(): void {
+    const value = this.newDiveceForm.getRawValue();
+    this.deviceService.addDevice(value as unknown as INewDeviceRequest)
+    .subscribe(res => {
+      console.log(res.message);
+      // 發布事件
+      this.dialogClosed.emit();
+    });
+  }
 }
