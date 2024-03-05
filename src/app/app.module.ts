64f5c41b38c30086576875b59ff0a993913597a7
cyrtz <cys20030203@gmail.com>
2024-03-05 17:40:44 +0800

    單位管理頁面樣式修改

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
@@ -9,10 +14,11 @@ import { DialogComponent } from './dialog/login-dialog/dialog.component';
 import { DeviceDetailComponent } from './device-detail/device-detail.component';
 import { DeviceManageComponent } from './device-manage/device-manage.component';
 import { NewDeviceDialogComponent } from './dialog/new-device-dialog/new-device-dialog.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { HttpClientModule } from '@angular/common/http';
+import { DeleteDeviceDialogComponent } from './dialog/delete-device-dialog/delete-device-dialog.component';
+import { EditDeviceDialogComponent } from './dialog/edit-device-dialog/edit-device-dialog.component';
+import { PlaceManageComponent } from './place-manage/place-manage.component';
+import { UserManageComponent } from './user-manage/user-manage.component';
+import { RegisterDialogComponent } from './dialog/register-dialog/register-dialog/register-dialog.component';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -23,7 +29,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatDividerModule } from '@angular/material/divider';
-import { NgApexchartsModule } from 'ng-apexcharts';
 import { MatTableModule } from '@angular/material/table';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -31,16 +36,11 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { DeleteDeviceDialogComponent } from './dialog/delete-device-dialog/delete-device-dialog.component';
-import { EditDeviceDialogComponent } from './dialog/edit-device-dialog/edit-device-dialog.component';
-// import { UserManagerComponent } from './user-manager/user-manager.component';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { RegisterDialogComponent } from './dialog/register-dialog/register-dialog/register-dialog.component';
-import { MAT_DATE_LOCALE, MatCommonModule } from '@angular/material/core';
 import { MatNativeDateModule } from '@angular/material/core';
-import { PlaceManageComponent } from './place-manage/place-manage.component';
-import { UserManageComponent } from './user-manage/user-manage.component';
-// import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { MAT_DATE_LOCALE, MatCommonModule } from '@angular/material/core';
+
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 @NgModule({
   declarations: [
@@ -53,7 +53,6 @@ import { UserManageComponent } from './user-manage/user-manage.component';
     NewDeviceDialogComponent,
     DeleteDeviceDialogComponent,
     EditDeviceDialogComponent,
-    // UserManagerComponent,
     RegisterDialogComponent,
     PlaceManageComponent,
     UserManageComponent
@@ -63,19 +62,18 @@ import { UserManageComponent } from './user-manage/user-manage.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
+    FlexLayoutModule,
+    HttpClientModule,
     MatCardModule,
     MatTabsModule,
     MatButtonModule,
     MatInputModule,
-    FlexLayoutModule,
     MatInputModule,
     MatIconModule,
     MatToolbarModule,
-    HttpClientModule,
     MatDialogModule,
     MatProgressBarModule,
     MatDividerModule,
-    NgApexchartsModule,
     MatTableModule,
     MatSnackBarModule,
     MatSidenavModule,
@@ -86,7 +84,7 @@ import { UserManageComponent } from './user-manage/user-manage.component';
     MatDatepickerModule,
     MatCommonModule,
     MatNativeDateModule,
-    // MatMomentDateModule
+    NgApexchartsModule,
   ],
   providers: [ {provide: MAT_DATE_LOCALE, useValue: 'zh-TW'}],
   bootstrap: [AppComponent]
