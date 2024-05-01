179b387e4723f2be5885b5fddf7cd8d31afb3248
cyrtz <cys20030203@gmail.com>
2024-05-01 17:50:09 +0800

    互動式地圖API接口完成

--- src/app/interactive-map/interactive-map.component.ts
+++ src/app/interactive-map/interactive-map.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { UnitManageService } from '../service/unit-manage/unit-manage.service';
 
 @Component({
   selector: 'app-interactive-map',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./interactive-map.component.scss']
 })
 export class InteractiveMapComponent {
+  show = true;
+  @ViewChild('id')
+  id!: ElementRef;
+  constructor(
+    private unitService: UnitManageService,
+  ) { }
 
+  // guid = this.id.nativeElement;
+  // guid = document.getElementById('id');
+
+  getDevice(placeGuid: string){
+    this.show = false;
+    console.log(placeGuid);
+    return this.unitService.searchDeviceByPlace(placeGuid).subscribe(res => {
+      console.log(res);
+    });
+  }
 }
